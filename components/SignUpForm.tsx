b75484895b21abcc6a39b2eef53d7054b500b097
Mohamed Elfar <melfar391@gmail.com>
2023-02-13 07:22:24 +0200

    first try to add google button

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -25,11 +25,11 @@ const SignUpForm = (props: FormProps) => {
                     <h1 className={styles.title}>Sign Up</h1>
                     <p>
                         already a member? <span><Link className='link--underlined' href='/login'>Log In</Link></span>
-                        </p>
-                    <form  action="#">
-                        <div className="btn--big--gray">
-                            <Link href='/'><Image className="icon" src="assets/googleLogo.svg" alt='' width={32} height={32}></Image>Sign Up with Google</Link>
-                        </div>
+                    </p>
+                    <button type="button" className={styles.btnGoogle} >
+                        Sign in with Google
+                    </button>
+                    <form action="#">
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
                         <button>Sign Up</button>
@@ -38,13 +38,13 @@ const SignUpForm = (props: FormProps) => {
             </div>
             <div className={styles.signupRight}>
                 <div className={styles.imageContainer}>
-                    <Image 
-                     src="/assets/Signup-Image-1.5x.png" 
-                     alt='' 
-                     fill
-                     className={styles.image}
-                     />
-                     
+                    <Image
+                        src="/assets/Signup-Image-1.5x.png"
+                        alt=''
+                        fill
+                        className={styles.image}
+                    />
+
                 </div>
             </div>
         </div>
