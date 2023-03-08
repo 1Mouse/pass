880965b1cc56588d8ec04e183174305f97806e9c
Mohamed Elfar <melfar391@gmail.com>
2023-03-08 08:11:14 +0200

    90% percent finished landing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,32 @@
+import styles from './hero.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section className={styles.block}>
+            <div className="container grid grid--1x2">
+                <header className={styles.heroContent}>
+                    <h1 className={styles.heading}>Practice mock interviews with FAANG engineers</h1>
+                    <p className={styles.tagLine}>Get better at algorithims, systems design and get detailed feedback about exactly what you need to work on.</p>
+                    <Link
+                        href="/login"
+                        className={styles.getStarted}
+                    >GET STARTED
+                    </Link>
+                </header>
+                <div className={styles.heroImage}>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src="/assets/heroImage.svg"
+                            alt=""
+                            fill
+                            className={styles.image}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
