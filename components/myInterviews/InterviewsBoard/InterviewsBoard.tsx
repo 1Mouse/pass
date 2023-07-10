fac14e5bb03162cc4ecd4487fd7dcd338cf8335e
Mohamed Elfar <melfar391@gmail.com>
2023-07-11 01:46:33 +0300

    before solving profile bug and photo

--- components/myInterviews/InterviewsBoard/InterviewsBoard.tsx
+++ components/myInterviews/InterviewsBoard/InterviewsBoard.tsx
@@ -8,7 +8,7 @@ import axios, { AxiosError } from 'axios';
 import InterviewCard from '../InterviewCard/InterviewCard';
 
 import IInterview from "@/lib/types/Interviews/IInterview";
-import {fireError} from '@/lib/utils/toasts';
+import { fireError } from '@/lib/utils/toasts';
 
 const InterviewsBoard = () => {
     // const hasMounted = useHasMounted();
@@ -22,16 +22,19 @@ const InterviewsBoard = () => {
 
     const [role] = useAuthStore(state => [state.user.role])
 
-    const filterAnInterview=(_id:string)=>{
-        const filtered=interviews?.filter(interview=>interview._id!==_id)
+    const filterAnInterview = (_id: string) => {
+        const filtered = interviews?.filter(interview => interview._id !== _id)
         setInterviews(filtered);
     }
     const fetchInterviews = async () => {
-        const role = switchRole ? "had" : "made";
-        console.log('fetchedQueries', role + "  " + switchStatus)
+        let type = switchRole ? "had" : "made";
+        if (role === 'interviewee') {
+            type = 'had';
+        }
+        console.log('fetchedQueries', type + "  " + switchStatus)
         try {
             setLoading(true);
-            const response = await axios.get(`${API_URL}/interviews?type=${role}&&status=${switchStatus}`,
+            const response = await axios.get(`${API_URL}/interviews?type=${type}&&status=${switchStatus}`,
                 {
                     headers: {
                         Authorization: `Bearer ${accessToken}`,
@@ -77,14 +80,14 @@ const InterviewsBoard = () => {
             </button>
             <div className={styles.statusContainer}>
                 <button
-                    className={styles.buttonStatus+' '+styles.yellow}
+                    className={styles.buttonStatus + ' ' + styles.yellow}
                     onClick={() => setSwitchStatus("pending")}
                     disabled={loading}
                 >
                     {loading ? 'loading...' : 'pending'}
                 </button>
                 <button
-                    className={styles.buttonStatus+' '+styles.green}
+                    className={styles.buttonStatus + ' ' + styles.green}
                     onClick={() => setSwitchStatus("confirmed")}
                     disabled={loading}
 
@@ -92,7 +95,7 @@ const InterviewsBoard = () => {
                     {loading ? 'loading...' : 'confirmed'}
                 </button >
                 <button
-                    className={styles.buttonStatus +" "+ styles.red}
+                    className={styles.buttonStatus + " " + styles.red}
                     onClick={() => setSwitchStatus("rejected")}
                     disabled={loading}
 
@@ -100,7 +103,7 @@ const InterviewsBoard = () => {
                     {loading ? 'loading...' : 'rejected'}
                 </button>
                 <button
-                    className={styles.buttonStatus+' '+styles.blue}
+                    className={styles.buttonStatus + ' ' + styles.blue}
                     onClick={() => setSwitchStatus("finished")}
                     disabled={loading}
 
@@ -110,12 +113,12 @@ const InterviewsBoard = () => {
             </div>
             {interviews?.length === 0 && <p className={styles.caughtUp}>You are all caught up</p>}
             <div className={styles.cardsContainer}>
-            {interviews?.map(interview => 
-            <InterviewCard 
-            key={interview._id} 
-            interview={interview} 
-            accessToken={accessToken} 
-            filterAnInterview={filterAnInterview}/>)}
+                {interviews?.map(interview =>
+                    <InterviewCard
+                        key={interview._id}
+                        interview={interview}
+                        accessToken={accessToken}
+                        filterAnInterview={filterAnInterview} />)}
             </div>
         </>
     );
