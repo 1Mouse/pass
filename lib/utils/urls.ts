7abcadc3137c16c3bde123d7941c8e383418aacf
Mohamed Elfar <melfar391@gmail.com>
2023-07-14 20:09:00 +0300

    updated backend with https deployment

--- lib/utils/urls.ts
+++ lib/utils/urls.ts
@@ -11,25 +11,25 @@
 //     isDevelopment ?'http://localhost:8080/api/v1':
 //         'http://localhost:8080/api/v1'
 
-// export const isProduction = process.env.NODE_ENV === 'production'
-// export const isDevelopment = process.env.NODE_ENV === 'development'
+export const isProduction = process.env.NODE_ENV === 'production'
+export const isDevelopment = process.env.NODE_ENV === 'development'
 
-// export const FRONT_URL = isProduction
-//     ? 'http://ec2-34-239-108-221.compute-1.amazonaws.com:3000/'
-//     : isDevelopment
-//         ? 'http://localhost:3000'
-//         : 'http://localhost:3000'
+export const FRONT_URL = isProduction
+    ? 'https://pass-1mouse.vercel.app'
+    : isDevelopment
+        ? 'http://localhost:3000'
+        : 'http://localhost:3000'
 
-// export const API_URL = isProduction ? 'http://ec2-34-207-95-142.compute-1.amazonaws.com/api/v1' :
-//     isDevelopment ? 'http://localhost:8080/api/v1' :
-//         'http://localhost:8080/api/v1'
-
-
-export const FRONT_URL = 'http://localhost:3000';
-// export const FRONT_URL = 'http://ec2-34-239-108-221.compute-1.amazonaws.com:3000';
+export const API_URL = isProduction ? 'https://passinterviews.me/api/v1' :
+    isDevelopment ? 'http://localhost:8080/api/v1' :
+        'http://localhost:8080/api/v1'
 
 
-export const API_URL = 'http://localhost:8080/api/v1';
-// export const API_URL ='http://ec2-34-207-95-142.compute-1.amazonaws.com/api/v1';
+// export const FRONT_URL = 'http://localhost:3000';
+// // export const FRONT_URL = 'http://ec2-34-239-108-221.compute-1.amazonaws.com:3000';
+
+
+// export const API_URL = 'http://localhost:8080/api/v1';
+// // export const API_URL ='http://ec2-34-207-95-142.compute-1.amazonaws.com/api/v1';
 
 
