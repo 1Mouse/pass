8fa50820746ee3723beeb8efeadaf3b6d559c0c9
Mohamed Elfar <melfar391@gmail.com>
2023-07-15 16:59:07 +0300

    updated domains

--- lib/utils/urls.ts
+++ lib/utils/urls.ts
@@ -15,12 +15,12 @@ export const isProduction = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
 
 export const FRONT_URL = isProduction
-    ? 'https://pass-1mouse.vercel.app'
+    ? 'http://passinterviews.com'
     : isDevelopment
         ? 'http://localhost:3000'
         : 'http://localhost:3000'
 
-export const API_URL = isProduction ? 'https://passinterviews.me/api/v1' :
+export const API_URL = isProduction ? 'https://backend.passinterviews.me/api/v1' :
     isDevelopment ? 'http://localhost:8080/api/v1' :
         'http://localhost:8080/api/v1'
 
